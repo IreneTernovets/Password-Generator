2cb6455ff05cdc73cf9782ee767c983cfca7091d
Irene <terna.legend@gmail.com>
2023-05-11 20:47:16 +0200

    Added function to copy password and generate password

--- index.js
+++ index.js
@@ -94,3 +94,44 @@ const characters = [
 
 let passwordFieldOne = document.getElementById("password-variant-one");
 let passwordFieldTwo = document.getElementById("password-variant-two");
+
+document.querySelectorAll(".copy-button").forEach(function (button) {
+  button.addEventListener("click", function () {
+    copyToClipboard(this.parentNode.querySelector("p"));
+  });
+});
+
+function copyToClipboard(element) {
+  var copyText = element.textContent;
+  var tempInput = document.createElement("input");
+  tempInput.setAttribute("value", copyText);
+  document.body.appendChild(tempInput);
+  tempInput.select();
+  document.execCommand("copy");
+  document.body.removeChild(tempInput);
+  alert("Copied to clipboard: " + copyText);
+}
+
+document
+  .getElementById("generate-button")
+  .addEventListener("click", function () {
+    passwordFieldOne.textContent = "";
+    passwordFieldTwo.textContent = "";
+    let passwords = generatePassword();
+    passwordFieldOne.textContent = passwords[0];
+    passwordFieldTwo.textContent = passwords[1];
+  });
+
+function generatePassword() {
+  let passwordOne = "";
+  let passwordTwo = "";
+  for (let i = 0; i < 10; i++) {
+    let randomIndex = Math.floor(Math.random() * characters.length);
+    passwordOne += characters[randomIndex];
+  }
+  for (let i = 0; i < 10; i++) {
+    let randomIndex = Math.floor(Math.random() * characters.length);
+    passwordTwo += characters[randomIndex];
+  }
+  return [passwordOne, passwordTwo];
+}
